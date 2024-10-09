6128e2f91e4f2db82b05d6ca6104d1138adcd365
shmp-dev <shmp.dev@gmail.com>
2024-10-09 17:52:58 +0900

    [algorithm]array_insert

---
+++ algorithm/array_insert.ts
@@ -0,0 +1,35 @@
+const initList = [0, 4, 0, 0, 0, 8, 10, 0];
+const inputList = [1, 2, 3, 5, 6, 7, 9]; // order
+let fixList: number[] = [];
+let inputIndex = 0;
+
+// input の最初の値が1の場合は、input の値が連続している限り output に追加する
+if (inputList[inputIndex] === 1) {
+  fixList.push(inputList[inputIndex]);
+  inputIndex++;
+
+  inputList.forEach(() => {
+    if (inputList[inputIndex - 1] + 1 === inputList[inputIndex]) {
+      // 一つ前の要素の値と、現在の要素の値の差を比較する
+      fixList.push(inputList[inputIndex]);
+      inputIndex++;
+    } else {
+      return;
+    }
+  });
+}
+
+initList.forEach((initNum) => {
+  inputList.forEach(() => {
+    if (inputIndex < inputList.length && initNum >= inputList[inputIndex]) {
+      fixList.push(inputList[inputIndex]);
+      inputIndex++;
+    } else {
+      return;
+    }
+  });
+
+  fixList.push(initNum);
+});
+// 正解は fixList = [1, 2, 0, 0, 3, 4, 5, 6, 0, 0, 7, 8, 9, 10]
+console.log(fixList);
